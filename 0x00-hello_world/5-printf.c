72cfc7ab67106c6070ae255a4ec330e8e800716e
mariamawit <mariamawitarega6@gmail.com>
2023-09-07 05:32:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+/**
+  *print "with proper grammar, but the outcome is a piece of art,"
+  */
+int main(void)
+{
+/* using printf*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
