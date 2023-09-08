101059e5c4d159b736105c03c2f98b5f565b7bb4
mariamawit <mariamawitarega6@gmail.com>
2023-09-08 05:55:55 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Description: 'alphabet in reverse'
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int a = 122;
 
-	while (a > 60)
-	{
+	do {
 		putchar(a);
 		a--;
-	}
+	} while (a > 97);
 	putchar('\n');
 	return (0);
 }
