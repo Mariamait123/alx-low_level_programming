bf2f1d2244c98d63646ab59a73f6f687de2b6028
mariamawit <mariamawitarega6@gmail.com>
2023-09-13 10:14:54 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * int _abs - computes the absolute value
+ *@a: its absolute value found
+ * Return: postive integer (Success)
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a*=-1;
+		return (a);
+	}
+	else
+		return (a);
+}
