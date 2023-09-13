8a908d29478bdc5470e03ee2a33af4c36edae171
mariamawit <mariamawitarega6@gmail.com>
2023-09-13 14:26:57 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 			if (n != 98)
 				printf("%d, ", n);
 			else
-				printf("%d ", n);
+				printf("%d\n", n);
 			n++;
 		}
 	}
@@ -24,7 +24,7 @@ void print_to_98(int n)
 			if (n != 98)
 				printf("%d, ", n);
 			else
-				printf("%d ", n);
+				printf("%d\n", n);
 			n--;
 		}
 	}
