a4ca5abc45510d782840a52136ddb27b48d600ee
mariamawit <mariamawitarega6@gmail.com>
2023-09-15 12:52:09 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "MAIN_H"
+/**
+  *_putchar - prints characters
+  *@c: character being printed
+  *Return: 1 when  succes
+  *on error, -1 is returned, errno is set appropriatly
+  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
