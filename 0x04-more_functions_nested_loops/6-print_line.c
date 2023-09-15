b92dc3594986bb2a0a733f41254dbb7acc344af8
mariamawit <mariamawitarega6@gmail.com>
2023-09-15 15:15:20 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *print_line - draw line
+  *@n: length of line
+  */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+			_putchar(_);
+	}
+	_putchar('\n');
+}
