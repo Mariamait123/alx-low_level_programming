bb9cc58d248c593d42835f67c8f9a94d5a6ec2db
mariamawit <mariamawitarega6@gmail.com>
2023-09-15 14:29:54 -0700

    task 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,10 +4,14 @@
   */
 void print_most_numbers(void)
 {
-	char i;
+	int i;
 
-	for (i = '0'; i < '10'; i++)
-			if (i != '2' && i != '4')
+	for (i = 48; i < 58; i++)
+	{
+			if (i != 50 && i != 52)
+			{
 				_putchar(i);
+			}
+	}
 	_putchar('\n');
 }
