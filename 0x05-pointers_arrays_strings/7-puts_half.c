fdfb0c81b1b6c995b8357c1b1eea57d0cb3a7727
mariamawit <mariamawitarega6@gmail.com>
2023-09-18 02:33:08 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *puts_half - prints half of string
+  *@str: input string
+  */
+void puts_half(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+		a++;
+	a /= 2;
+	while (str[a] != '\0')
+	{
+		_putchar(str[a];
+		a++;
+	}
+}
