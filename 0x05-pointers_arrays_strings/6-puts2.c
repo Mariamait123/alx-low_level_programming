09536019dacb1cb2704d7b13d988ddecbeba0bbd
mariamawit <mariamawitarega6@gmail.com>
2023-09-18 02:09:23 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *puts2 - prints every other character of a string
+  *@str: input string
+  */
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
