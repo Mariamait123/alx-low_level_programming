0ed7030510abb3b9518be0da325c7234209e4b92
mariamawit <mariamawitarega6@gmail.com>
2023-09-20 01:46:21 -0700

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  *_strncpy - copies a string
+  *@dest: string being edited
+  *@src: string being copied
+  *@n: determin how much src is copied
+  *Return: new string
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0, i = 0;
+
+	while (i < n)
+	{
+		if (src[i] == '\0')
+		{
+			dest[a] = src[i];
+			break;
+		}
+		else
+			dest[a] = src[i];
+		a++;
+		i++;
+	}
+	return (dest);
+}
