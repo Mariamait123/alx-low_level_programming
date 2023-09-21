3d11ee17e71ae7f7e8c7b3c198a007c0d74d28e5
mariamawit <mariamawitarega6@gmail.com>
2023-09-21 01:05:20 -0700

    100-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,15 +7,19 @@
 char *rot13(char *c)
 {
 	int a, b;
-	char m1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char m2[] = "abcdefghijklmnopqrstuvwxyz";
+	char m1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rt[] = "NOPQRSTUVQXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (a = 0; c[a] != '\0'; a++)
 	{
-		for (b = 0; b < 26; b++)
+		for (b = 0; b < 52; b++)
 		{
-			if (c[a] == m1[b] || c[a] == m2[b])
-				c[a] += 13;
+			if (c[a] == m1[b])
+			{
+				c[a] = rt[b];
+				break;
+			}
 		}
 	}
+	return (c);
 }
