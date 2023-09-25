9e2b4ab00be4791602d7ca8b79ecb3f760c67c4a
mariamawit <mariamawitarega6@gmail.com>
2023-09-25 10:09:07 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *print_chessboard - prints the chessboard
+  *@a: input char
+  */
+void print_chessboard(char (*a)[8])
+{
+	int s, d;
+
+	for (s = 0; s < 8; s++)
+	{
+		for (d = 0; d < 8; d++)
+			_putchar(a[s][d]);
+		_putchar('\n');
+	}
+}
