c1f01dec20c52ffa632f6112fe0feeadba2aa0da
mariamawit <mariamawitarega6@gmail.com>
2023-10-02 12:55:46 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *main - Entery point
+  *@argc: The number of command line arguments
+  *@argv: An array containing the program command line arguments
+  *Return: Always 0
+  */
+int main (int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
