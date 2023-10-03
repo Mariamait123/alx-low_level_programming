50190304f4cdfad1e7f1a448f635006f0b5a281a
mariamawit <mariamawitarega6@gmail.com>
2023-10-03 01:05:21 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,12 +11,11 @@ char *_strdup(char *str)
 	char *c;
 	int a = 0, b;
 
-	if (c == NULL)
+	if (str == NULL)
 		return (NULL);
 	while (str[a] != '\0')
 		a++;
 	c = malloc(sizeof(char) * (a + 1));
-	if (str == NULL)
 		return (NULL);
 	for (b = 0; str[b]; b++)
 		c[b] = str[b];
