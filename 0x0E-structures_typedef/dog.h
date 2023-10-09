03b1beafa40e1e49885176e7ef5f984d9d51808d
mariamawit <mariamawitarega6@gmail.com>
2023-10-09 13:09:18 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+  *struct dog - desription of dog
+  *@name: name of dog
+  *@age: age of dog
+  *@owner: owner of dog
+  *
+  *Description: Longer description
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+  *dog_t - typedef for struct dog
+  */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
