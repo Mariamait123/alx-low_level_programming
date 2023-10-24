770c9f28104a97cd7a3f7b8cfc9247045df1a0f3
mariamawit <mariamawitarega6@gmail.com>
2023-10-24 03:18:20 -0700

    0-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,6 +11,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp = *head;
 	listint_t *c = NULL;
 
+	if (*head == NULL)
+		return (-1);
 	if (index == 0)
 	{
 		*head = (*head)->next;
